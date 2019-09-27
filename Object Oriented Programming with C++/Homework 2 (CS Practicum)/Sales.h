d837fd7859ff78b8d2f350a59441a3368f024266
stoyankirov <55755559+stoyankirov@users.noreply.github.com>
2019-09-27 13:24:47 +0300

    Update Sales.h

--- Object Oriented Programming with C++/Homework 2 (CS Practicum)/Sales.h
+++ Object Oriented Programming with C++/Homework 2 (CS Practicum)/Sales.h
@@ -7,6 +7,10 @@ class Sales:virtual public Employee
 		Sales(const char* name, int practise,int numOfSales);
 		virtual int getSalary() const;
 		virtual Employee* clone() const { return new Sales(*this); };
+		
+		void setNumOfSales(int num);
+		
+		int getNumOfSales() const;
 	private:
 		int _numOfSales;
 };
